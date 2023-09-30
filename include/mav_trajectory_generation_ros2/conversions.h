a1e9e21f2fb960836d629d0d513a6b6fea193809
Mohamed Abdelkader <mohamedashraf123@gmail.com>
2023-10-01 00:22:08 +0300

    Initial implementation of trajectory_sampler_node

--- include/mav_trajectory_generation_ros2/conversions.h
+++ include/mav_trajectory_generation_ros2/conversions.h
@@ -23,6 +23,8 @@
 #include <geometry_msgs/msg/quaternion.hpp>
 #include <geometry_msgs/msg/vector3.hpp>
 #include <nav_msgs/msg/odometry.hpp>
+#include <trajectory_msgs/msg/multi_dof_joint_trajectory.hpp>
+#include <trajectory_msgs/msg/multi_dof_joint_trajectory_point.hpp>
 
 #include <mav_trajectory_generation_ros2/msg/polynomial_segment.hpp>
 #include <mav_trajectory_generation_ros2/msg/polynomial_trajectory.hpp>
@@ -116,7 +118,81 @@ inline void polynomialTrajectoryMsgFromEigen(
   }
 }
 
+inline void msgMultiDofJointTrajectoryPointFromEigen(
+    const mav_msgs::EigenTrajectoryPoint& trajectory_point,
+    trajectory_msgs::msg::MultiDOFJointTrajectoryPoint* msg) {
+  assert(msg != NULL);
 
+  msg->time_from_start.sec = trajectory_point.time_from_start_ns / 1'000'000'000; 
+  msg->time_from_start.nanosec = trajectory_point.time_from_start_ns % 1'000'000'000;
+  msg->transforms.resize(1);
+  msg->velocities.resize(1);
+  msg->accelerations.resize(1);
+
+  mav_msgs::vectorEigenToMsg(trajectory_point.position_W,
+                   &msg->transforms[0].translation);
+  mav_msgs::quaternionEigenToMsg(trajectory_point.orientation_W_B,
+                       &msg->transforms[0].rotation);
+  mav_msgs::vectorEigenToMsg(trajectory_point.velocity_W, &msg->velocities[0].linear);
+  mav_msgs::vectorEigenToMsg(trajectory_point.angular_velocity_W,
+                   &msg->velocities[0].angular);
+  mav_msgs::vectorEigenToMsg(trajectory_point.acceleration_W,
+                   &msg->accelerations[0].linear);
+  mav_msgs::vectorEigenToMsg(trajectory_point.angular_acceleration_W,
+                   &msg->accelerations[0].angular);
+}
+
+inline void msgMultiDofJointTrajectoryFromEigen(
+    const mav_msgs::EigenTrajectoryPointVector& trajectory, const std::string& link_name,
+    trajectory_msgs::msg::MultiDOFJointTrajectory* msg) {
+  assert(msg != NULL);
+
+  if (trajectory.empty()) {
+    std::cout << "EigenTrajectoryPointVector is empty." << std::endl;
+    return;
+  }
+
+  msg->joint_names.clear();
+  msg->joint_names.push_back(link_name);
+  msg->points.clear();
+
+  for (const auto& trajectory_point : trajectory) {
+    trajectory_msgs::msg::MultiDOFJointTrajectoryPoint point_msg;
+    msgMultiDofJointTrajectoryPointFromEigen(trajectory_point, &point_msg);
+    msg->points.push_back(point_msg);
+  }
+}
+
+inline void msgMultiDofJointTrajectoryFromEigen(
+    const mav_msgs::EigenTrajectoryPointVector& trajectory,
+    trajectory_msgs::msg::MultiDOFJointTrajectory* msg) {
+  msgMultiDofJointTrajectoryFromEigen(trajectory, "base_link", msg);
+}
+
+inline void msgMultiDofJointTrajectoryFromEigen(
+    const mav_msgs::EigenTrajectoryPoint& trajectory_point, const std::string& link_name,
+    trajectory_msgs::msg::MultiDOFJointTrajectory* msg) {
+  assert(msg != NULL);
+  trajectory_msgs::msg::MultiDOFJointTrajectoryPoint point_msg;
+  msgMultiDofJointTrajectoryPointFromEigen(trajectory_point, &point_msg);
+
+  msg->joint_names.clear();
+  msg->points.clear();
+  msg->joint_names.push_back(link_name);
+  msg->points.push_back(point_msg);
+}
+
+inline void msgMultiDofJointTrajectoryFromEigen(
+    const mav_msgs::EigenTrajectoryPoint& trajectory_point,
+    trajectory_msgs::msg::MultiDOFJointTrajectory* msg) {
+  msgMultiDofJointTrajectoryFromEigen(trajectory_point, "base_link", msg);
+}
+
+inline void msgMultiDofJointTrajectoryFromEigen(
+    const mav_msgs::EigenTrajectoryPointVector& trajectory,
+    trajectory_msgs::msg::MultiDOFJointTrajectory* msg) {
+  msgMultiDofJointTrajectoryFromEigen(trajectory, "base_link", msg);
+}
 
 }  // namespace mav_planning_msgs
 
---
+++ nodes/trajectory_sampler_node.cpp
@@ -0,0 +1,183 @@
+#include <Eigen/Eigen>
+#include <chrono>
+#include <functional>
+#include <memory>
+#include <string>
+#include "rclcpp/rclcpp.hpp"
+
+#include <mav_trajectory_generation_ros2/conversions.h>
+#include <mav_trajectory_generation_ros2/eigen_mav_msgs.h>
+#include <mav_trajectory_generation_ros2/msg/polynomial_segment.hpp>
+#include <mav_trajectory_generation_ros2/msg/polynomial_trajectory.hpp>
+#include <trajectory_msgs/msg/multi_dof_joint_trajectory.hpp>
+
+#include <mav_trajectory_generation_ros2/polynomial.h>
+#include <mav_trajectory_generation_ros2/trajectory_sampling.h>
+#include <mav_trajectory_generation_ros2/ros_conversions.h>
+
+// deprecated
+#include <mav_trajectory_generation_ros2/msg/polynomial_segment4_d.hpp>
+#include <mav_trajectory_generation_ros2/msg/polynomial_trajectory4_d.hpp>
+
+using namespace std::chrono_literals;
+using std::placeholders::_1;
+
+class TrajectorySamplerNode : public rclcpp::Node
+{
+ public:
+  TrajectorySamplerNode();
+  ~TrajectorySamplerNode();
+
+ private:
+  void pathSegmentsCallback(
+      const mav_trajectory_generation_ros2::msg::PolynomialTrajectory& segments_message);
+  void pathSegments4DCallback(
+      const mav_trajectory_generation_ros2::msg::PolynomialTrajectory4D& segments_message);
+  // bool stopSamplingCallback(std_srvs::Empty::Request& request,
+  //                           std_srvs::Empty::Response& response);
+  // void commandTimerCallback(const ros::TimerEvent&);
+  void processTrajectory();
+
+  void commandTimerCallback();
+
+  rclcpp::Subscription<mav_trajectory_generation_ros2::msg::PolynomialTrajectory>::SharedPtr trajectory_sub_;
+  rclcpp::Subscription<mav_trajectory_generation_ros2::msg::PolynomialTrajectory4D>::SharedPtr trajectory4D_sub_;
+
+  rclcpp::Publisher<trajectory_msgs::msg::MultiDOFJointTrajectory>::SharedPtr command_pub_;
+
+  rclcpp::TimerBase::SharedPtr timer_;
+  bool run_timer_;
+
+  // ros::ServiceServer stop_srv_;
+  rclcpp::Time start_time_;
+
+  // Service client for getting the MAV interface to listen to our sent
+  // commands.
+  // ros::ServiceClient position_hold_client_;
+
+  // Flag whether to publish entire trajectory at once or not.
+  bool publish_whole_trajectory_;
+  // Trajectory sampling interval.
+  double dt_;
+  // Time at currently published trajectory sample.
+  double current_sample_time_;
+
+  // The trajectory to sub-sample.
+  mav_trajectory_generation::Trajectory trajectory_;
+};
+
+/////////////////////// Class definition ////////////////////////
+
+TrajectorySamplerNode::TrajectorySamplerNode(): Node("trjectory_sampler_node"),
+  run_timer_(false),
+  publish_whole_trajectory_(false),
+  dt_(0.01),
+  current_sample_time_(0.0)
+{
+  trajectory_sub_ = this->create_subscription<mav_trajectory_generation_ros2::msg::PolynomialTrajectory>(
+      "path_segments", rclcpp::SensorDataQoS(), std::bind(&TrajectorySamplerNode::pathSegmentsCallback, this, _1)); 
+  trajectory4D_sub_ = this->create_subscription<mav_trajectory_generation_ros2::msg::PolynomialTrajectory4D>(
+      "path_segments_4D", rclcpp::SensorDataQoS(), std::bind(&TrajectorySamplerNode::pathSegments4DCallback, this, _1));
+
+  command_pub_ = this->create_publisher<trajectory_msgs::msg::MultiDOFJointTrajectory>("command/trajectory", 10);
+
+  timer_ = this->create_wall_timer(
+            std::chrono::milliseconds(static_cast<int64_t>(dt_*1000)),
+            std::bind(&TrajectorySamplerNode::commandTimerCallback, this) );
+}
+
+TrajectorySamplerNode::~TrajectorySamplerNode(){ /* Destructor*/}
+
+void TrajectorySamplerNode::pathSegments4DCallback(
+      const mav_trajectory_generation_ros2::msg::PolynomialTrajectory4D& segments_message)
+{
+  if (segments_message.segments.empty()) {
+    RCLCPP_WARN(this->get_logger(),"Trajectory sampler: received empty waypoint message");
+    return;
+  } else {
+    RCLCPP_INFO(this->get_logger(),"Trajectory sampler: received %lu waypoints",
+             segments_message.segments.size());
+  }
+
+    bool success = mav_trajectory_generation::polynomialTrajectoryMsgToTrajectory(
+        segments_message, &trajectory_);
+    if (!success) {
+      return;
+    }
+    processTrajectory();
+}
+
+void TrajectorySamplerNode::pathSegmentsCallback(
+      const mav_trajectory_generation_ros2::msg::PolynomialTrajectory& segments_message)
+{
+  if (segments_message.segments.empty()) {
+    RCLCPP_WARN(this->get_logger(), "Trajectory sampler: received empty waypoint message");
+    return;
+  } else {
+    RCLCPP_INFO(this->get_logger(), "Trajectory sampler: received %lu waypoints",
+             segments_message.segments.size());
+  }
+
+    bool success = mav_trajectory_generation::polynomialTrajectoryMsgToTrajectory(
+        segments_message, &trajectory_);
+    if (!success) {
+      return;
+    }
+    processTrajectory();
+}
+
+void TrajectorySamplerNode::processTrajectory()
+{
+  // Call the service call to takeover publishing commands.
+  // if (position_hold_client_.exists()) {
+  //   std_srvs::Empty empty_call;
+  //   position_hold_client_.call(empty_call);
+  // }
+
+  if (publish_whole_trajectory_) {
+    // Publish the entire trajectory at once.
+    mav_msgs::EigenTrajectoryPoint::Vector trajectory_points;
+    mav_trajectory_generation::sampleWholeTrajectory(trajectory_, dt_,
+                                                     &trajectory_points);
+    trajectory_msgs::msg::MultiDOFJointTrajectory msg_pub;
+    mav_planning_msgs::msgMultiDofJointTrajectoryFromEigen(trajectory_points, &msg_pub);
+    command_pub_->publish(msg_pub);
+  } else {
+    // publish_timer_.start();
+    current_sample_time_ = 0.0;
+    run_timer_ = true;
+    start_time_ = this->now();
+    
+  }
+}
+
+void TrajectorySamplerNode::commandTimerCallback()
+{
+  if(!run_timer_)
+    return;
+
+  if (current_sample_time_ <= trajectory_.getMaxTime()) {
+    trajectory_msgs::msg::MultiDOFJointTrajectory msg;
+    mav_msgs::EigenTrajectoryPoint trajectory_point;
+    bool success = mav_trajectory_generation::sampleTrajectoryAtTime(
+        trajectory_, current_sample_time_, &trajectory_point);
+    if (!success) {
+      // publish_timer_.stop();
+      run_timer_ = false;
+    }
+    mav_planning_msgs::msgMultiDofJointTrajectoryFromEigen(trajectory_point, &msg);
+    // Convert time_from_start to seconds and nanoseconds
+    int64_t seconds = static_cast<int64_t>(current_sample_time_);
+    int64_t nanoseconds = static_cast<int64_t>((current_sample_time_ - seconds) * 1e9);
+    msg.points[0].time_from_start.sec = seconds;
+    msg.points[0].time_from_start.nanosec = nanoseconds;
+
+    command_pub_->publish(msg);
+    current_sample_time_ += dt_;
+    
+  } else {
+    // publish_timer_.stop();
+    run_timer_ = false;
+  }
+
+}
